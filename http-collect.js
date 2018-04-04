912d39b06631aa990518e6ccd9e8ac49a7c88474
pradesh-kumar <pradeshkumar1@gmail.com>
2018-04-05 01:08:37 +0530

    Adding files

---
+++ http-collect.js
@@ -0,0 +1,12 @@
+var concatStream = require('concat-stream');
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, (response) => {
+  response.setEncoding('utf8');
+  response.pipe(concatStream((data) => {
+    console.log(data.length);
+    console.log(data);
+  }));
+});
---
+++ time-server.js
@@ -0,0 +1,17 @@
+var net = require('net');
+
+var server = net.createServer((socket) => {
+	socket.end(getFormattedCurrentTime() + "\n"); 
+});
+
+server.listen(process.argv[2]);
+
+var getFormattedCurrentTime = () => {
+	var now = new Date();
+	return [now.getFullYear(), formatNumber(now.getMonth() + 1), formatNumber(now.getDate())].join("-")
+	+ " " + [formatNumber(now.getHours()), formatNumber(now.getMinutes())].join(":");
+}
+
+var formatNumber = (number) => {
+	return number < 10 ? "0" + number : number;
+}
